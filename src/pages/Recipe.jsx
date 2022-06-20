856a548f884aca246102ee6ada226a1b4e6ab77a
hosan30 <50867996+hosan30@users.noreply.github.com>
2022-06-21 02:47:17 +0600

    nn

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,12 +1,27 @@
 import {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import {useParams} from 'react-router-dom';
-
 import React from 'react'
 
+
 function Recipe() {
+
+  let params = useParams();
+  const [details , setDetails] = useState({});
+
+const key = '83eb4662ee9b495b8b922c39052652ee';
+const fetchDetails = async () =>{
+const data = await fetch (`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${key}`)
+const detailData = await data.json();
+setDetails(detailData);
+}
+useEffect(() => {
+ fetchDetails();
+},[params.name]
+)
+
   return (
-    <div>Recipe</div>
+    <div>{details.title}</div>
   )
 }
 
