c81f8816dd784824c4447ad0c7ed720015ae6124
hosan30 <50867996+hosan30@users.noreply.github.com>
2022-06-21 06:59:17 +0600

    jhj
    jg

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -21,8 +21,52 @@ useEffect(() => {
 );
 
   return (
-    <div>{details.title}</div>
+    <DetailsWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt={details.title} />
+      </div>
+      <Info>
+        <Button>Instruction</Button>
+        <Button>Ingredients</Button>
+
+      </Info>
+    </DetailsWrapper>
   )
 }
+const DetailsWrapper = styled.div`
+margin-top: 10rem;
+margin-bottom: 5rem;
+display: flex;
+
+.active{
+background: linear-gradient(35deg, #494949, #313131);
+color: white;
+}
+h2{
+margin-bottom: 2rem;
+
+}
+li{
+font-size: 1rem;
+line-height: 2rem;
+
+}
+ul{
+margin-top: 2rem;
+}
+`;
+const Button = styled.div`
+padding: 1rem 2rem;
+color: #313131;
+background: white;
+border: 2px solid black;
+margin-right: 2rem;
+font-weight: 600;
+`
+const Info = styled.div`
+margin-left: 10rem;
+
+`
 
 export default Recipe
